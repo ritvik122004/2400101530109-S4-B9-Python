f64ca92499b1dff012fab230a166cb5ac016c909
Ritvik Singh <ritviksingh439@gmail.com>
2026-03-31 07:31:41 +0000

    change

--- secondproject/src/App.jsx
+++ secondproject/src/App.jsx
@@ -4,32 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  function callfun(){
+    alert("Function Call");
+  }
+  function Apple(){
+    alert("You are click on apple button");
+  }
+  function Banana(){
+    alert("You are click on banana button");
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Welcome in new project</h1>
+      <button onClick={callfun}>OK</button>
+      <button onClick={Apple}>APPLE</button>
+      <button onClick={Banana}>BANANA</button>
     </>
   )
+
 }
 
 export default App
